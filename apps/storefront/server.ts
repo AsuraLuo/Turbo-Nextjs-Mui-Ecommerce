247a1d1683ae688d93de366f9b5460575755d3ac
caesar <caesar.luo@silksoftware.com>
2023-01-05 09:02:17 +0800

    chore: custom koa server

--- apps/storefront/server.ts
+++ apps/storefront/server.ts
@@ -1,6 +1,7 @@
-import { createServer } from 'http'
 import { parse } from 'url'
 import next from 'next'
+import Koa from 'koa'
+import Router from '@koa/router'
 
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
@@ -8,14 +9,27 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
+  const server = new Koa()
+  const router = new Router()
 
-  console.warn(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
+  router.all('(.*)', async (ctx: any) => {
+    const { req, res } = ctx
+    const parsedUrl = parse(req.url, true)
+    await handle(req, res, parsedUrl)
+    ctx.respond = false
+  })
+
+  server.use(async (ctx: any, nextFunc: any) => {
+    ctx.res.statusCode = 200
+    await nextFunc()
+  })
+
+  server.use(router.routes())
+  server.listen(port, () => {
+    console.warn(
+      `> Server listening at http://localhost:${port} as ${
+        dev ? 'development' : process.env.NODE_ENV
+      }`
+    )
+  })
 })

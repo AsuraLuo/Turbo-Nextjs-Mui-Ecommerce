2f153b8fec355acfe383003578ed09f937885260
caesar <caesar.luo@silksoftware.com>
2023-02-10 14:53:48 +0800

    chore: build

--- apps/storefront/next.config.js
+++ apps/storefront/next.config.js
@@ -1,5 +1,4 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')
-
+const isProd = process.env.NODE_ENV === 'production'
 const isAnalyzer = process.env.REACT_APP_BUNDLE_VISUALIZE === '1'
 
 module.exports = () => {
@@ -7,13 +6,23 @@ module.exports = () => {
    * @type {import('next').NextConfig}
    */
   const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
+    basePath: '',
     compress: false,
     distDir: '.next',
     generateEtags: false,
     pageExtensions: ['tsx', 'ts'],
+    poweredByHeader: false,
+    reactStrictMode: true,
+    swcMinify: true,
+    trailingSlash: false,
     transpilePackages: ['@ecommerce/ui'],
+    compiler: {
+      reactRemoveProperties: isProd,
+      removeConsole: isProd
+    },
+    eslint: {
+      ignoreDuringBuilds: true
+    },
     webpack: (config) => {
       // Important: return the modified config
       return config
@@ -24,7 +33,7 @@ module.exports = () => {
 
   if (isAnalyzer)
     plugins.push(
-      withBundleAnalyzer({
+      require('@next/bundle-analyzer')({
         enabled: true
       })
     )

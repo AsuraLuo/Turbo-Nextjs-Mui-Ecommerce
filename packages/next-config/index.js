4e182b7a45c4abb54e369cd6500eed4b5ece219c
caesar <caesar.luo@silksoftware.com>
2023-07-21 15:06:58 +0800

    chore: nextjs swc compile

--- packages/next-config/index.js
+++ packages/next-config/index.js
@@ -29,9 +29,27 @@ module.exports = (pkg = {}) => {
     assetPrefix: isProd ? CDN_URL : undefined,
     transpilePackages: ['lodash-es', '@ocommerce/ui'],
     compiler: {
-      emotion: true,
       reactRemoveProperties: isProd,
-      removeConsole: isProd
+      removeConsole: isProd,
+      emotion: {
+        sourceMap: true,
+        autoLabel: 'dev-only',
+        labelFormat: '[local]',
+        importMap: {
+          '@mui/system': {
+            styled: {
+              canonicalImport: ['@emotion/styled', 'default'],
+              styledBaseImport: ['@mui/system', 'styled']
+            }
+          },
+          '@mui/material/styles': {
+            styled: {
+              canonicalImport: ['@emotion/styled', 'default'],
+              styledBaseImport: ['@mui/material/styles', 'styled']
+            }
+          }
+        }
+      }
     },
     eslint: {
       ignoreDuringBuilds: isProd

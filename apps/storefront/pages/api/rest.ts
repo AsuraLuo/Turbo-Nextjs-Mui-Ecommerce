9ec82a2fdd6a894c5e4a7637d5aa547afe1a8464
caesar <caesar.luo@silksoftware.com>
2023-08-13 09:25:30 +0800

    chore: api cache

--- apps/storefront/pages/api/rest.ts
+++ apps/storefront/pages/api/rest.ts
@@ -1,24 +1,32 @@
-import { NextApiRequest, NextApiResponse } from 'next/types'
-import { createProxyMiddleware } from 'http-proxy-middleware'
+import { createProxyMiddleware, responseInterceptor } from 'http-proxy-middleware'
+import type { NextApiRequest, NextApiResponse } from 'next/types'
+
+const apiProxy: any = createProxyMiddleware({
+  target: process.env.REACT_APP_API_URL,
+  changeOrigin: true,
+  secure: true,
+  pathRewrite: {
+    '^/api/rest': '/'
+  },
+  selfHandleResponse: true,
+  onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+    // set headers manually
+    res.setHeader(
+      'cache-control',
+      'public, max-age=3600, s-maxage=60, stale-while-revalidate=86400'
+    )
+
+    return responseBuffer
+  })
+})
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  const apiProxy: any = createProxyMiddleware({
-    target: process.env.REACT_APP_API_URL,
-    changeOrigin: true,
-    secure: true,
-    pathRewrite: {
-      '^/api/rest': '/'
-    }
-  })
-
   apiProxy(req, res, (result: any) => {
     if (result instanceof Error) {
       throw result
     }
 
-    throw new Error(
-      `Request '${req.url}' is not proxied! We should never reach here!`
-    )
+    throw new Error(`Request '${req.url}' is not proxied! We should never reach here!`)
   })
 }
 

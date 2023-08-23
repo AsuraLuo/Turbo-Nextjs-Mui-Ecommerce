b654533a8b9f1a7dd840a1dec92450e8f1dcc3a3
caesar <caesar.luo@silksoftware.com>
2023-08-23 09:10:29 +0800

    chore: mui text

--- apps/admin/components/IFormInput/iFormInput.tsx
+++ apps/admin/components/IFormInput/iFormInput.tsx
@@ -5,21 +5,32 @@ import type { FC } from 'react'
 import type { ControllerProps } from 'react-hook-form'
 import type { TextFieldProps } from '@mui/material/TextField'
 
-interface IFormInputProps extends Omit<TextFieldProps, 'required'> {
-  name: string
+type ControllerRules = ControllerProps['rules']
+
+type InputRules = Exclude<ControllerRules, 'required'> & {
   required?: boolean
-  controllerProps?: ControllerProps
 }
 
-const IFormInput: FC<IFormInputProps> = ({ name, required = false, controllerProps, ...props }) => {
+interface IFormInputProps extends Omit<TextFieldProps, 'required'> {
+  name: string
+  rules?: InputRules
+  controllerProps?: Omit<ControllerProps, 'name' | 'control' | 'rules'>
+}
+
+const IFormInput: FC<IFormInputProps> = ({ name, rules = {}, controllerProps, ...props }) => {
   const { control } = useFormContext<any>()
+  const { required = false, ...rest } = rules
+  const controlRules: ControllerRules = {
+    required: required && 'This field is required.',
+    ...rest
+  }
 
   return (
     <Controller
       {...controllerProps}
       name={name}
       control={control}
-      rules={{ required: 'This field is required.' }}
+      rules={controlRules}
       render={({ field, formState: { errors } }) => {
         const { name: fieldName } = field
         return (
--- apps/admin/components/LoginPage/loginPage.tsx
+++ apps/admin/components/LoginPage/loginPage.tsx
@@ -4,13 +4,13 @@ import type { SubmitHandler } from 'react-hook-form'
 import IFormContext from '@/components/IFormContext'
 import IFormInput from '@/components/IFormInput'
 
-interface IFormData {
+type IFormValues = {
   firstname: string
   lastname: string
 }
 
 const LoginPage = () => {
-  const defaultValues: IFormData = {
+  const defaultValues: IFormValues = {
     firstname: '',
     lastname: ''
   }
@@ -19,22 +19,24 @@ const LoginPage = () => {
   //   control,
   //   name: 'test'
   // })
+  const validEmail = (value: string) => {
+    const regexp: RegExp =
+      /^([a-z0-9,!\#\$%&'\*\+\/=\?\^_`\{\|\}~-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z0-9,!\#\$%&'\*\+\/=\?\^_`\{\|\}~-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*@([a-z0-9-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z0-9-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*\.(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]){2,})$/i
 
-  const onFinish: SubmitHandler<IFormData> = (values) => {
+    return regexp.test(value) || 'Please enter a valid email address.'
+  }
+
+  const onFinish: SubmitHandler<IFormValues> = (values) => {
     console.info(values)
   }
 
   return (
-    <IFormContext
-      defaultValues={defaultValues}
-      formProps={{ reValidateMode: 'onSubmit' }}
-      onFinish={onFinish}
-    >
+    <IFormContext defaultValues={defaultValues} onFinish={onFinish}>
       <div>
-        <IFormInput name="firstname" required />
+        <IFormInput name="firstname" rules={{ required: true, validate: validEmail }} />
       </div>
       <div>
-        <IFormInput name="lastname" required />
+        <IFormInput name="lastname" rules={{ required: true }} />
       </div>
       {/* {fields.map((field, index) => {
         const name = `test.${index}.firstname`
